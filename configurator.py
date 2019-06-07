1aa5db230a4305f1d81021e2db7c1b279e47cbeb
Dragca Kvasnickova <uldrijanova@seznam.cz>
2019-06-08 00:51:57 +0200

    class method new

--- configurator.py
+++ configurator.py
@@ -6,12 +6,27 @@ class Service:
     def __init__(self, name):
         self.name = name
 
+    @classmethod
+    def new(cls, service_dict):
+        """
+        From definition of service in given dict 
+        creates ActiveService or DeletedService instance.
+        @param service_dict - dict
+        @returns new instance of service
+        """
+
+        if "deleted" in service_dict and service_dict["deleted"] == True:
+            return DeletedService(service_dict["name"])
+
+        return ActiveService(**service_dict)
+
+
     def to_json(self):
         if self.deleted:
             return json.dumps({'name': self.name})
 
-        result = {"name": self.name, "ip": self.ip_address, "port": self.port,
-                  "type": self.service_type, "network_policy": self.network_policy}
+        result = {"name": self.name, "ip": self.ip, "port": self.port,
+                  "type": self.type, "network_policy": self.network_policy}
 
         if self.certificate_name is not None:
             result["certificate_name"] = self.certificate_name
@@ -30,19 +45,19 @@ class DeletedService(Service):
 
 
 class ActiveService(Service):
-    def __init__(self, name, ip_address, port, service_type, network_policy, certificate_name=None):
+    def __init__(self, name, ip, port, type, network_policy, certificate_name=None, servers=None):
         self.deleted = False
 
         super().__init__(name)
-        self.ip_address = ip_address
+        self.ip = ip
         self.port = port
-        self.service_type = service_type
+        self.type = type
         self.network_policy = network_policy
         self.certificate_name = certificate_name
 
     def validate(self):
         super().validate()
-        if self.service_type == "https":
+        if self.type == "https":
             if self.certificate_name is None or self.certificate_name == "":
                 raise ValueError("Service type 'https' requires certificate name.")
 
@@ -53,4 +68,4 @@ if __name__ == "__main__":
             service_dict = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-    service = 
+    service = Service.new(service_dict)
